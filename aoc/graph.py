e878914a33331153bba1beca44bc5053ff26f32a
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-12-22 20:35:06 +0100

    Implement `heuristic` for A* search

--- aoc/graph.py
+++ aoc/graph.py
@@ -11,6 +11,8 @@ def shortest_path(
     start: Node,
     nbs: Callable[[Node], Iterator[Tuple[Node, float]]],
     callback: Callable[[Node, float], bool] = lambda n, d: False,
+    heuristic: Callable[[Node], float] = lambda n: 0,
+    revisit: bool = False,
 ) -> Tuple[Dict[Node, float], Dict[Node, Node]]:
     """Dijkstra's algorithm to compute all shortest paths starting at `start`.
 
@@ -24,6 +26,12 @@ def shortest_path(
             every iteration with the current node and the true shortest path distance to
             that node. If this function returns `True`, the algorithm will stop.
             Otherwise, the algorithm will continue.
+        heuristic (Callable[[Node], float], optional): An heuristic that lower
+            bounds the distance to the end node. It should also be monotonic, meaning
+            that it never decreases by more than the edge weight. It is it not
+            monotonic, you should set `revisit` to `True`.
+        revisit (bool, optional): Allow the algorithm to revisit the same node
+            arbitrarily many times instead of just once. Defaults to `False`.
 
     Returns:
         tuple[dict[Node, float], dict[Node, Node]]:
@@ -36,7 +44,7 @@ def shortest_path(
     seen: Set[Node] = set()
 
     q: Queue[Tuple[float, Node]] = PriorityQueue()
-    q.put((0, start))
+    q.put((heuristic(start), start))
 
     while not q.empty():
         d1, n1 = q.get()
@@ -44,7 +52,8 @@ def shortest_path(
         # This needs to execute first to ensure that `d1` is right.
         if n1 in seen:
             continue
-        seen.add(n1)
+        elif not revisit:
+            seen.add(n1)
 
         if callback(n1, d1):
             break
@@ -59,6 +68,6 @@ def shortest_path(
             if alt < dist.get(n2, float("inf")):
                 dist[n2] = alt
                 prev[n2] = n1
-                q.put((dist[n2], n2))
+                q.put((dist[n2] + heuristic(n2), n2))
 
     return dist, prev
